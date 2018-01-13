3a5d127074e48be5bf8ba40dc56aa370bc74d0ea
Tin Chu <mchu.tinho@gmail.com>
2018-01-12 20:38:25 -0600

    making POST request

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
 const app = express();
+
+mongoose.Promise = global.Promise;
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/muber');  
+}
+
+app.use(bodyParser.json());
+routes(app);
 
 module.exports = app;
---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+const DriversController = require('../controllers/drivers_controller');
+
+module.exports = app => {
+  /**
+   * Watch for incoming requests of method GET
+   * to the route http://localhost:3050/api
+   */
+  app.get('/api', DriversController.greeting);
+
+  app.post('/api/drivers', DriversController.create);
+};
