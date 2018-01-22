948f58a367cce211928cece9b5489957c51f38e6
Tin Chu <mchu.tinho@gmail.com>
2018-01-22 07:52:24 -0600

    final 2dsphere

--- test/test_helper.js
+++ test/test_helper.js
@@ -13,6 +13,7 @@ beforeEach(done => {
   const { drivers } = mongoose.connection.collections;
 
   drivers.drop()
+    .then(() => drivers.ensureIndex({'geometry.coordinates': '2dsphere'}))
     .then(() => done())
     .catch(() => done());
 });
